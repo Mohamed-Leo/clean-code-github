8373d546e886816941e2a787dc1d54435629906f
Mohamed-Leo <mohamedlewandoski2018@gmail.com>
2024-10-01 03:18:14 +0300

    doc: add global rules, next rules and eslint with prettier

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
+/**
+ * ${1:Description placeholder}
+ *
+ * @type {"hello"}
+ */
 const hello = "hello";
 
+/**
+ * ${1:Description placeholder}
+ *
+ * @returns {${2:*}}
+ */
 const helloFunc = () => {
 	return console.log(hello);
 };
